8a771ca23b872980c244777f3262b7f180638201
Youssef Hussein <youssef.m.hussein@gmail.com>
2025-07-16 00:16:45 +0300

    (Fix) SHA256 hashes

--- solarboat.rb
+++ solarboat.rb
@@ -6,10 +6,10 @@ class Solarboat < Formula
 
     if OS.mac?
         url "https://github.com/devqik/solarboat/releases/download/v#{version}/solarboat-x86_64-apple-darwin.tar.gz"
-        sha256 "sha256:55fa3f63d5bd671049c01a0f5bbb2a8173d020e7b5f513937150bde58c71fdc0"
+        sha256 "55fa3f63d5bd671049c01a0f5bbb2a8173d020e7b5f513937150bde58c71fdc0"
     elsif OS.linux?
         url "https://github.com/devqik/solarboat/releases/download/v#{version}/solarboat-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "sha256:3b9bdbbe38483e32d77b25ca13f16d0acd4b4269a0cfcb39fcf996ad023fb8ab"
+        sha256 "3b9bdbbe38483e32d77b25ca13f16d0acd4b4269a0cfcb39fcf996ad023fb8ab"
     end
 
     def install

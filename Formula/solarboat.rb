35367ce085f470169638c1c15bad5ca3688fa4a1
Youssef Hussein <youssef.m.hussein@gmail.com>
2025-08-01 21:17:08 +0300

    Update solarboat.rb

--- Formula/solarboat.rb
+++ Formula/solarboat.rb
@@ -1,15 +1,15 @@
 class Solarboat < Formula
     desc "Modern CLI for Infrastructure as Code and GitOps workflows"
     homepage "https://solarboat.io"
-    version "0.8.1"
+    version "0.8.3"
     license "BSD-3-Clause"
 
     if OS.mac?
         url "https://github.com/devqik/solarboat/releases/download/v#{version}/solarboat-x86_64-apple-darwin.tar.gz"
-        sha256 "d7b5e07c587cd4881d4c611e44a416059dbb2c90e87238833a2df1a754679073"
+        sha256 "4f3ae99941e22629d8d1ea826dd92f810a01aeb473fb22a7d6721dbbb6ab28a1"
     elsif OS.linux?
         url "https://github.com/devqik/solarboat/releases/download/v#{version}/solarboat-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "a5673a4fa323c997ebfbba430814eb6cf54c7f12d6f84f47e6aed99cd2925ad7"
+        sha256 "c471030bd65d547bf8e5ae8acf125976cc65ad703c6a611ed66323b775783224"
     end
 
     def install

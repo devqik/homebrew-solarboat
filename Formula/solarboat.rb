0963e0eb9c103f69ceb91047598ffa0ac1f24148
Youssef Hussein <youssef.m.hussein@gmail.com>
2025-08-13 01:02:07 +0300

    Update solarboat.rb

--- Formula/solarboat.rb
+++ Formula/solarboat.rb
@@ -1,7 +1,7 @@
 class Solarboat < Formula
     desc "Modern CLI for Infrastructure as Code and GitOps workflows"
     homepage "https://solarboat.io"
-    version "0.8.4"
+    version "0.8.5"
     license "BSD-3-Clause"
 
     if OS.mac?
